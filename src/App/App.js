435c43e7449af5f0d9c0c15870513ce973c21bf7
Mary Rys <rys.mary@gmail.com>
2019-01-07 21:22:30 -0600

    logout working

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-// import firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import connection from '../helpers/data/connection';
 import Auth from '../components/Auth/Auth';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
 
 import './App.scss';
+import authRequests from '../helpers/data/authRequests';
 
 class App extends Component {
   state = {
@@ -16,27 +18,46 @@ class App extends Component {
   componentDidMount() {
     connection();
 
-    // this.removeListener = firebase.auth().onAuthStateChanged((user) => {
-    //   if (user) {
-    //     this.setState({
-    //       authed: true,
-    //     });
-    //   } else {
-    //     this.setState({
-    //       authed: false,
-    //     });
-    //   }
-    // });
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+        });
+      } else {
+        this.setState({
+          authed: false,
+        });
+      }
+    });
+  }
 
-    // isAuthenticated = (username) => {
-    //   this.setState({ authed: true, })
-    // }
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  isAuthenticated = (username) => {
+    this.setState({ authed: true });
   }
 
   render() {
+    const { authed } = this.state;
+    const logoutClickEvent = () => {
+      authRequests.logoutUser();
+      this.setState({ authed: false });
+    };
+
+    if (!authed) {
+      return (
+        <div className="App">
+            <MyNavbar isAuthed={authed} logoutClickEvent={logoutClickEvent}/>
+            <Auth isAuthenticated={this.isAuthenticated}/>
+        </div>
+      );
+    }
     return (
       <div className="App">
-          <Auth />
+        <MyNavbar isAuthed={authed} logoutClickEvent={logoutClickEvent} />
+          <p>You are authenticated</p>
       </div>
     );
   }
