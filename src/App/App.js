bf4708eba37d7102cb0bd176163e9fcedddda367
Mary Rys <rys.mary@gmail.com>
2019-01-07 19:48:04 -0600

    logging in

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-
+// import firebase from 'firebase/app';
+import 'firebase/auth';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import logo from './logo.svg';
+
+import connection from '../helpers/data/connection';
 import Auth from '../components/Auth/Auth';
 
 import './App.scss';
 
 class App extends Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    connection();
+
+    // this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+    //   if (user) {
+    //     this.setState({
+    //       authed: true,
+    //     });
+    //   } else {
+    //     this.setState({
+    //       authed: false,
+    //     });
+    //   }
+    // });
+
+    // isAuthenticated = (username) => {
+    //   this.setState({ authed: true, })
+    // }
+  }
+
   render() {
     return (
       <div className="App">
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import authRequests from '../../helpers/data/authRequests';
 
 class Auth extends React.Component {
+  authenticateUser = (e) => {
+    e.preventDefault();
+    authRequests.authenticate().then(() => {
+    }).catch(err => console.error('there was an error with auth', err));
+  }
+
   render() {
     return (
       <div className="Auth">
-      <button className="btn btn-danger">Login</button>
+      <button className="btn btn-danger" onClick={this.authenticateUser}>Login</button>
       </div>
     );
   }
