f78842e0a7c698c57f5a0efe30198f9b533afe9d
Mary Rys <rys.mary@gmail.com>
2019-01-07 19:12:32 -0600

    added auth component

---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Auth extends React.Component {
+  render() {
+    return (
+      <div className="Auth">
+      <button className="btn btn-danger">Login</button>
+      </div>
+    );
+  }
+}
+
+export default Auth;
