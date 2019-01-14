1ac37d97f67411edf376850ccfd496bd5b088d60
Mary Rys <rys.mary@gmail.com>
2019-01-14 13:21:35 -0600

    messages printing with message limit

---
+++ src/components/MessagesItem/MessagesItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import './MessagesItem.scss';
+
+class MessagesItem extends React.Component {
+  render() {
+    const { message } = this.props;
+    return (
+      <div className="messagesItem">
+        <div className="container">
+          <div className="row">
+            <h3>{message.message}</h3>
+            <img src={message.photo} alt="userImage"></img>
+          </div>
+          <h6>{message.userName}</h6>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default MessagesItem;
--- src/components/pages/Messages/Messages.js
+++ src/components/pages/Messages/Messages.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import './Messages.scss';
+import MessagesItem from '../../MessagesItem/MessagesItem';
+
+import smashRequests from '../../../helpers/data/smashRequests';
 
 class Messages extends React.Component {
+  state = {
+    messages: [],
+  }
+
+  getMessagesForPage = () => {
+    smashRequests.getAllMessagesWithUserInfo()
+      .then((messages) => {
+        if (messages.length > 1) {
+          messages.shift(messages.legth - 1, messages.length);
+        }
+        this.setState({ messages });
+      })
+      .catch(err => console.error('error with messages GET', err));
+  }
+
+  componentDidMount() {
+    this.getMessagesForPage();
+  }
+
   render() {
+    const { messages } = this.state;
+    const messagesItemComponents = messages.map(message => (
+      <MessagesItem
+      message={message}
+      key={message.id}
+      />
+    ));
     return (
-      <div className="Messages">
+      <div className="Messages mx-auto" width="200px">
+      <div className="container">
       <h2>Messages Page</h2>
+      {messagesItemComponents}
+      </div>
       </div>
     );
   }
